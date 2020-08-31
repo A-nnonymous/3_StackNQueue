172475b4c748e8a797268077cceb9966832dbf91
joesmith <617428699@qq.com>
2020-08-31 19:33:46 +0800

    Added linked/array stack operation

--- head.h
+++ head.h
@@ -1,1 +1,45 @@
 #pragma once
+using namespace std;
+#define MAXSIZE 10
+
+typedef struct LNode {
+	int data;
+	struct LNode* next;
+}LNode;
+
+void array_stack_push(int array[], int& stack_top, int push_item)
+{
+	array[++stack_top] = push_item;
+}
+
+int array_stack_pop(int array[], int& stack_top)
+{
+	return array[stack_top--];
+}
+
+int linked_stack_pop(LNode*& head)
+{
+	int ans;
+	LNode* top;
+	if (head->next == NULL)return 0;
+	top = head->next;	ans = top->data;
+	top = top->next;
+	free(head->next); head->next = top;
+	return ans;
+}
+
+void linked_stack_push(LNode*& head, int item)
+{
+	LNode* insertion;
+	insertion = (LNode*)malloc(sizeof(LNode));
+	insertion->data = item;
+	insertion->next = head->next;
+	head->next = insertion;
+}
+
+void linked_queue_out()
+{
+}
+void linked_queue_in()
+{
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 #include"head.h"
+using namespace std;
 
 void main()
 {
+	int temp;
+	int Array_Stack1[MAXSIZE]; int ASTop1 = -1;
+	LNode* linked_stack_head; linked_stack_head = (LNode*)malloc(sizeof(LNode));
+	/*----------------array stack test-------------------------*/
+	/*for (int i = 0; i < 10; i++)
+	{
+		array_stack_push(Array_Stack1, ASTop1, i);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		temp = array_stack_pop(Array_Stack1, ASTop1);
+		cout << "The " << i << " th output item is:" << temp << endl;
+	}
+	*/
+	/*----------------array stack test-------------------------*/
+	/*--------------------linked stack test--------------------*/
+
+	/*for (int i = 0; i < 4; i++)
+	{
+		cout << "Please input the inserting item:"; cin >> temp;
+		linked_stack_push(linked_stack_head, temp);
+	}
+	cout << "INPUT OVER, START POPING..." << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "The " << i << " th item is:" << linked_stack_pop(linked_stack_head) << endl;
+	}
+
+	/*--------------------linked stack test--------------------*/
 }
